0b9770ad0b4f7068aeac38c25526c65e3965fccd
comstudy21joon <76138542+comstudy21joon@users.noreply.github.com>
2020-12-17 18:10:07 +0900

    Update Calc.java

--- src/main/java/sig/oss/calc/Calc.java
+++ src/main/java/sig/oss/calc/Calc.java
@@ -5,11 +5,11 @@ public class Calc {
 	
     public void add(int x, int y) {
         r = x+y;
-    }
+    } // add method 주석문 추가
     
     public void sub(int x, int y) {
     	r = x-y;
-    }
+    } // substraction method 주석문 추가
     
     public int getResult() {
     	return r;
